6f29364ce9573684d035f101d01c71ed45b5a274
Rosberg00 <erik.rosberg@fsd.se>
2020-03-22 19:29:14 +0100

    While loop som frågar ifall du vill generera nytt password

--- Pgen/Pgen/Pgen.py
+++ Pgen/Pgen/Pgen.py
@@ -1,20 +1,24 @@
 import random
 import string
 
+while True:
+    def rndpwd():
+        while True:
+            Length = input("Hur många karaktärer ska ditt lösenord ha ")
+            try:
+                Length = int(Length)
+                break
+            except ValueError:
+              print("inte ett nummer försök igen")
 
-def rndpwd():
-    print("din input length")
+        letters = string.ascii_letters + string.digits + string.punctuation
 
-    while True:
-        Length = input()
-        try:
-            Length = int(Length)
-            break
-        except ValueError:
-          print("inte ett numer försök igen")
+        return ''.join(random.choice(letters) for i in range(Length))
+    clear
+    print ("Ditt lösenord är " )
+    print (rndpwd())
+    print ( )
+    print ("Vill du generera ett nytt lösen ord  ")
+    if input("y / n ") == "n":
+        break
 
-    letters = string.ascii_letters + string.digits + string.punctuation
-
-    return ''.join(random.choice(letters) for i in range(Length))
-
-print ("Ditt lösenord är ", rndpwd() )
