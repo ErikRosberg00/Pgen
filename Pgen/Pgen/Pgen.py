f720303490611e5806e004d6d0fa20fb2c6eb91a
Rosberg00 <erik.rosberg@fsd.se>
2020-03-22 18:46:41 +0100

    tage

--- Pgen/Pgen/Pgen.py
+++ Pgen/Pgen/Pgen.py
@@ -1,13 +1,9 @@
 import random
 import string
 
-print("Hur många karaktärer ska ditt lösenord ha")
-Length = int(input())
-print(Length)
-
+Length = int(input("Hur många karaktärer ska ditt lösenord ha "))   
 def Rndpwd():
     Pwdcrs = string.ascii_letters + string.digits + string.punctuation
     return ''.join(random.choice(Pwdcrs) for i in range(Length))
 
-print ("Lösenord ", Rndpwd() )
-
+print ("Ditt lösenord är", Rndpwd() )
