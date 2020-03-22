abb33d02d321a59ed5b7d937fa297684735cab95
tage <tagebe8@gmail.com>
2020-03-22 21:29:33 +0100

    Fixade med ja och nej funktion

--- Pgen/Pgen/Pgen.py
+++ Pgen/Pgen/Pgen.py
@@ -1,24 +1,34 @@
 import random
 import string
+import sys
 
-while True:
+def main():
     def rndpwd():
-        while True:
-            Length = input("Hur många karaktärer ska ditt lösenord ha ")
-            try:
-                Length = int(Length)
-                break
-            except ValueError:
-              print("inte ett nummer försök igen")
-
-        letters = string.ascii_letters + string.digits + string.punctuation
-
-        return ''.join(random.choice(letters) for i in range(Length))
-    clear
+            while True:
+                Length = input("Hur många karaktärer ska ditt lösenord ha ")
+                try:
+                    Length = int(Length)
+                    break
+                except ValueError:
+                  print("inte ett nummer försök igen")
+        
+            letters = string.ascii_letters + string.digits + string.punctuation
+        
+            return ''.join(random.choice(letters) for i in range(Length))
     print ("Ditt lösenord är " )
     print (rndpwd())
     print ( )
     print ("Vill du generera ett nytt lösen ord  ")
-    if input("y / n ") == "n":
-        break
+    def jaynay():
+        print ("Y / N")
+        x = input()
+        if x == "n":
+            print ("programet stänger")
+            exit(0)
+        elif x == "y":
+             main()
+        else:
+            jaynay()
+    jaynay()
+main()
 
